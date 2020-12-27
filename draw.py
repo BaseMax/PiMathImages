ca1407b48fe15be1a2aafa76f8fe2dea875b7ff0
Max Base <MaxBaseCode@Gmail.Com>
2020-12-27 16:45:46 +0330

    Adding beautiful colors

--- draw.py
+++ draw.py
@@ -12,3 +12,21 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # im = Image.open("target.jpg")
 im = None
 draw = None
+
+colors = [
+    # "#05b4ff", # blue
+    "#ffb22e",
+    "#ef3c38",
+    "#fbf018",
+    "#f53b3c",
+    # "#05b4ff", # blue
+    "#cbf11f",
+    "#1b3c9d",
+    "#ff6117",
+    "#93ce34",
+    "#c6e420",
+    "#fec337",
+    "#fcc73d",
+    "#05b4ff",
+    "#23c244",
+]
